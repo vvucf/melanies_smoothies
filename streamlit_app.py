1871d598aabe8e3c456c3899960965544424eb61
VV <va626766@ucf.edu>
2025-06-20 00:32:27 -0400

    Update streamlit_app.py
    api

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
